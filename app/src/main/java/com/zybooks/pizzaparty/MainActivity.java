97877d905f34744c559a58cfbdc18f3909e62004
Tanya Selvog <tanyaselvog@gmail.com>
2022-05-10 13:57:10 -0700

    added code to MainActivity

--- app/src/main/java/com/zybooks/pizzaparty/MainActivity.java
+++ app/src/main/java/com/zybooks/pizzaparty/MainActivity.java
@@ -1,14 +1,38 @@
+
+
 package com.zybooks.pizzaparty;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    public final int SLICES_PER_PIZZA = 8;
+
+    private EditText mNumAttendEditText;
+    private TextView mNumPizzasTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mNumAttendEditText = findViewById(R.id.attendEditText);
+        mNumPizzasTextView = findViewById(R.id.answerTextView);
+    }
+
+    public void calculateClick(View view) {
+        String numAttendStr = mNumAttendEditText.getText().toString();
+        int numAttend = Integer.parseInt(numAttendStr);
+
+        int slicesPerPerson = 4;
+        int totalPizzas = (int) Math.ceil(numAttend * slicesPerPerson /
+                (double) SLICES_PER_PIZZA);
+        mNumPizzasTextView.setText("Total pizzas: " + totalPizzas);
     }
 }
+
+
